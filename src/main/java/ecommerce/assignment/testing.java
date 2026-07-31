53fd0d456a2328738a20084cb48b97c42cd48e5f
shivanip557 <prasadshivani557@gmail.com>
2026-07-31 20:30:37 +0530

    done

---
+++ src/main/java/ecommerce/assignment/testing.java
@@ -0,0 +1,15 @@
+package ecommerce.assignment;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String USERNAME = "shivaniprasad_qgl83b";
+		String ACCESS_KEY = "zcVT94Cycy29hzWqz5yS";
+
+		String URL = "https://" + USERNAME + ":" + ACCESS_KEY +
+		             "@hub-cloud.browserstack.com/wd/hub";
+
+	}
+
+}
